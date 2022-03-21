0529f314df529417d4d67e0eb8bdb7ed64855d80
Donny <doniwahyu14@gmail.com>
2022-03-21 12:09:09 +0700

    task 4 selesai

---
+++ task/task4.js
@@ -0,0 +1,20 @@
+function divideAndSort(nomor) {
+  let result;
+
+  nomor = nomor.toString();
+  result = nomor.split("0");
+
+  result.map((value, index) => {
+    // value = value.split("");
+    value = [...value]; //mengubah string menjadi array di tiap angka
+    result[index] = value.sort((a, b) => a - b).join("");
+    //mengurutkan array dengan method sort
+    // dan menggabungkan array menjadi string menggunakan method join
+  });
+
+  // mengubah array result menjadi satu string
+  result = result.join("");
+  console.log(`bilangan awal : ${nomor}`);
+  console.log(`setelah proses divide n order : ${result}`);
+}
+divideAndSort(32301230934);
